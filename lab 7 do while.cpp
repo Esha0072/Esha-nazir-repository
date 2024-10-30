2f51423fd15ac2f30c2daf8861440645dacba932
Esha0072 <eshanazir76@gmail.com>
2024-10-30 09:43:14 +0500

    Add files via upload

---
+++ lab 7 do while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i=1;
+	long f=1;
+	cout<<"enter your number";
+	cin>>num;
+	do{
+		factorial=f*i;
+		i++;
+}
+while(i<=n)
+cout<<"factorial is"<<f;
+return 0;
+}
